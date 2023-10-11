8ede41d90a5cfdce9b115b0ed199032e23ffd228
varun thaker <vthakar3@gmail.com>
2023-10-11 16:38:19 +0200

    changed express router

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require("express");
+const PORT = 8000 || process.env.PORT;
+const dbConnect = require("./db/connect.js");
+const Product = require("./db/models/Product.js");
+
 const app = express();
 app.use(express.json());
 
-const dbConnect = require("./db/connect.js");
 dbConnect();
-const routes = require("./routes/routes.js");
-app.use("/api/v1", routes);
-
-const PORT = 8000 || process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Please Route to /api/v1/products for data");
+});
+
+app.get("/api/v1/products", async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    res.status(200).json(allProducts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/v1/products/:id", async (req, res) => {
+  try {
+    const productWithID = await Product.findById(req.params.id);
+    res.status(200).json(productWithID);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
 });
+
+module.exports = app;
