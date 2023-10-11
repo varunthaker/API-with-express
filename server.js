4e10bc0fd82bb693526f3d55feabd13c0c304f9a
varun thaker <vthakar3@gmail.com>
2023-10-11 10:13:24 +0200

    updated route

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ app.use(express.json());
 const dbConnect = require("./db/connect.js");
 dbConnect();
 const routes = require("./routes/routes.js");
-app.use("/api", routes);
+app.use("/api/v1", routes);
 
 const PORT = 8000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
